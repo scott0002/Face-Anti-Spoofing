bec4ffb78f4eb78817bdee5168750d1d3f703781
tsai1247 <jeff29cc91079@gmail.com>
2020-04-18 20:19:33 +0800

    Update readimg.m

--- spoofClassification/readimg.m
+++ spoofClassification/readimg.m
@@ -1,5 +1,5 @@
 %??directory?file_path?????????x?label y
-function [x,y]=readimg(file_path)
+function [x,y]=readimg(file_path);
 x=[];
 y=[];
 img_path_list = dir(strcat(file_path,'*.png'));
@@ -8,7 +8,7 @@ if img_num > 0
         for j = 1:img_num 
             image_name = img_path_list(j).name;
             image =  imread(strcat(file_path,image_name));
-            if img_num==1000
+            if mod(j, 100) ==0
                 fprintf('%d %d %s\n',i,j,strcat(file_path,image_name));
             end
             %image???? 
